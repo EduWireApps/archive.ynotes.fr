b7f0f82c0714105ec0c4427f97451d027efbc2ef
taokann.one <dev@taokann.com>
2020-05-09 10:48:36 +0200

    Add addMail.php with GPG encryption. pubkey is gitignored. Pubkey must be src/assets/jsonlines.asc

---
+++ src/scripts/addMail.php
@@ -0,0 +1,18 @@
+<?php
+if (isset($_POST['mail'])) {
+    try {
+    $file = '../assets/adress.txt';
+    $mail =  $_POST['mail'] . "\n";
+    putenv("GNUPGHOME=/tmp");
+    $pubkey = file_get_contents("../assets/jsonlines.asc");
+    $res = gnupg_init();
+    $rtv = gnupg_import($res, $pubkey);
+    $rtv = gnupg_addencryptkey($res, "0223BC767B9BBCCD1B596FAB7D0F8CBFDFE252FE");
+    $enc = gnupg_encrypt($res, $mail);
+    file_put_contents($file, $enc,  FILE_APPEND);
+}catch (Exception $e)
+{
+    throw $e;
+}
+}
+?>
