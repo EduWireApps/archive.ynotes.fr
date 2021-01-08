40feb0f58d37f3218d99328a441e6ca60b1bd7ac
NerofleLincoln <nerofle.0810.lincoln@gmail.com>
2021-01-08 22:26:09 +0100

    fixed arrow animation on hero section

--- components/HomeSections/Hero.vue
+++ components/HomeSections/Hero.vue
@@ -53,7 +53,7 @@
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              class="h-12 w-12 animate-float xyz-nested"
+              class="h-12 w-12 animate-bounce xyz-nested"
               stroke="currentColor"
             >
               <path
