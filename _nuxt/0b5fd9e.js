82f8f6678fc517619e165260816482bc8daa75f6
NerofleLincoln <NerofleLincoln@users.noreply.github.com>
2021-01-11 21:18:56 +0000

    deploy: 1d67f6d9a16be982b28cc512844453f2418b2c3d

---
+++ _nuxt/0b5fd9e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{318:function(n,t,e){"use strict";e.r(t);var l={},c=e(8),component=Object(c.a)(l,(function(){var n=this.$createElement;return(this._self._c||n)("div",[this._v("SUPPORT")])}),[],!1,null,null,null);t.default=component.exports}}]);
