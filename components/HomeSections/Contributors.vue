4f2058067b2f7f64add07f45d13fee0b2da092e7
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2021-04-17 12:22:28 +0200

    add gabriel faudou as a contributor and rework Contributors section

--- components/HomeSections/Contributors.vue
+++ components/HomeSections/Contributors.vue
@@ -2,98 +2,79 @@
   <section class="py-12 bg-white">
     <Container>
       <XyzTransition appear-visible duration="auto">
-        <div class="flex flex-col h-full py-16 lg:flex-row">
-          <div class="flex flex-col justify-center flex-1 mx-6 mb-6">
-            <div class="text-left">
-              <h2
-                class="text-3xl font-extrabold leading-8 tracking-tight text-center text-gray-900 sm:text-5xl md:text-left xyz-nested"
-                xyz="fade down-1 delay-3 duration-15"
-                v-html="content.title"
-              ></h2>
-              <p
-                class="max-w-2xl mt-8 mb-4 text-lg leading-8 text-justify text-gray-700 lg:text-xl xyz-nested"
-                xyz="fade down-1 delay-6 duration-15"
-                v-html="content.content"
-              ></p>
-              <a
-                class="inline-flex items-center text-lg font-medium tracking-wide text-transparent transition-colors duration-200 bg-clip-text bg-gradient-to-br from-indigo-600 to-purple-600 focus:outline-none hover:opacity-80 xyz-nested"
-                :href="
-                  require('~/assets/content/contact.json').repo +
-                    '#contributeurs-'
-                "
-                target="_blank"
-                rel="noopener noreferrer"
-                xyz="fade down-1 delay-8 duration-15"
-              >
-                <span v-html="content.link.text"></span>
-                <Zondicon
-                  icon="cheveron-right"
-                  class="w-5 h-5 text-purple-600 fill-current"
-              /></a>
-            </div>
+        <div class="w-fit-content">
+          <div class="text-center">
+            <h2
+              class="mt-2 text-3xl font-extrabold leading-8 tracking-tight text-gray-900 sm:text-5xl xyz-nested"
+              xyz="fade down-1 delay-3 duration-15"
+              v-html="content.title"
+            ></h2>
+            <p
+              class="max-w-3xl my-4 text-xl text-gray-500 lg:mx-auto xyz-nested"
+              xyz="fade down-1 delay-6 duration-15"
+              v-html="content.content"
+            ></p>
+            <a
+              class="inline-flex items-center text-lg font-medium tracking-wide text-transparent transition-colors duration-200 bg-clip-text bg-gradient-to-br from-indigo-600 to-purple-600 focus:outline-none hover:opacity-80 xyz-nested"
+              :href="
+                require('~/assets/content/contact.json').repo +
+                  '#contributeurs-'
+              "
+              target="_blank"
+              rel="noopener noreferrer"
+              xyz="fade down-1 delay-8 duration-15"
+            >
+              <span v-html="content.link.text"></span>
+              <Zondicon
+                icon="cheveron-right"
+                class="w-5 h-5 text-purple-600 fill-current"
+            /></a>
           </div>
-          <div
-            class="inline-flex items-center justify-center flex-1 xyz-nested"
-            xyz="fade delay-8"
-          >
-            <div class="grid grid-cols-1 gap-4 pb-12">
-              <XyzConflict
-                v-for="(c, index) in contributors"
-                :key="index"
-                class="transition-transform transform"
-                xyz="fade stagger-4"
-                endClass="md:hover:translate-x-4"
-              >
-                <a
-                  class="flex flex-col bg-purple-100 rounded-2xl"
-                  :href="c.user.html_url"
-                  target="_blank"
-                  rel="noopener noreferrer"
+          <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap gap-y-8 gap-x-2 mt-8">
+            <a
+              v-for="(c, index) in contributors"
+              :key="index"
+              :href="c.user.html_url"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="flex flex-col items-center space-y-4 py-2"
+            >
+              <img
+                :src="
+                  c.user.require_image
+                    ? require(`~/assets/${c.user.avatar_url}`)
+                    : c.user.avatar_url
+                "
+                class="block w-24 h-24 rounded-full shadow-md"
+                :alt="c.user.login + ' avatar'"
+              />
+              <div class="text-center font-semibold">
+                <div class="text-gray-800 text-lg">{{ c.user.login }}</div>
+                <div
+                  class="text-transparent bg-clip-text bg-gradient-to-br from-indigo-600 to-purple-600"
                 >
-                  <div
-                    class="bg-gradient-to-tr from-indigo-500 to-purple-500 h-16 rounded-t-2xl"
-                  >
-                    <img
-                      :src="c.user.avatar_url"
-                      class="block w-24 h-24 p-1 bg-purple-100 rounded-full ml-8 mt-4"
-                      :alt="c.user.login + ' avatar'"
-                    />
-                  </div>
-                  <div
-                    class="flex flex-col flex-1 justify-center py-4 px-8 ml-28 relative"
-                  >
+                  <span
+                    v-if="c.customSentence !== ''"
+                    v-html="c.customSentence"
+                  ></span>
+                  <span v-else>
                     <span
-                      class="text-xl font-medium leading-6 text-space-500"
-                      v-html="c.user.login"
+                      v-html="c.contributed.app ? 'Application' : null"
                     ></span>
-                    <p class="text-lg text-gray-700 mt-2">
-                      <span
-                        v-if="c.customSentence !== ''"
-                        v-html="c.customSentence"
-                      ></span>
-                      <span v-else>
-                        Contributeur
-                        <span
-                          v-html="c.contributed.app ? 'à l\'application' : null"
-                        ></span>
-                        <span
-                          v-html="
-                            c.contributed.app && c.contributed.website
-                              ? ' et '
-                              : null
-                          "
-                        ></span>
-                        <span
-                          v-html="
-                            c.contributed.website ? 'au site internet' : null
-                          "
-                        ></span>
-                      </span>
-                    </p>
-                  </div>
-                </a>
-              </XyzConflict>
-            </div>
+                    <span
+                      v-html="
+                        c.contributed.app && c.contributed.website
+                          ? ' / '
+                          : null
+                      "
+                    ></span>
+                    <span
+                      v-html="c.contributed.website ? 'Site web' : null"
+                    ></span>
+                  </span>
+                </div>
+              </div>
+            </a>
           </div>
         </div>
       </XyzTransition>
@@ -107,11 +88,11 @@ export default {
     return {
       excludedUsers: this.content.fetch.excludedUsers,
       customUsers: this.content.fetch.customUsers,
-      contributors: [],
+      contributors: []
     };
   },
   props: {
-    content: Object,
+    content: Object
   },
   methods: {
     addContributor(el) {
@@ -120,14 +101,15 @@ export default {
           login: el.login,
           html_url: el.html_url,
           avatar_url: el.avatar_url,
+          require_image: el.require_image
         },
         contributed: {
           app: el.app,
-          website: el.website,
+          website: el.website
         },
-        customSentence: el.customSentence,
+        customSentence: el.customSentence
       });
-    },
+    }
   },
   async created() {
     const contributors = [
@@ -136,9 +118,9 @@ export default {
       ),
       await this.$axios.$get(
         "https://api.github.com/repos/ynotesapp/ynotes-website/contributors"
-      ),
+      )
     ];
-    this.customUsers.forEach((el) => {
+    this.customUsers.forEach(el => {
       this.addContributor({
         login: el.login,
         html_url: el.html_url,
@@ -146,27 +128,29 @@ export default {
         app: el.app,
         website: el.website,
         customSentence: el.customSentence,
+        require_image: el.require_image
       });
     });
     let ctbtApp = contributors[0].filter(
-      (c) => !this.excludedUsers.includes(c.login)
+      c => !this.excludedUsers.includes(c.login)
     );
-    ctbtApp.forEach((el) => {
+    ctbtApp.forEach(el => {
       this.addContributor({
         login: el.login,
         html_url: el.html_url,
         avatar_url: el.avatar_url,
         app: true,
-        website: contributors[1].some((c) => c.login === el.login),
+        website: contributors[1].some(c => c.login === el.login),
         customSentence: "",
+        require_image: false
       });
     });
     let ctbtWebsite = contributors[1].filter(
-      (c) =>
+      c =>
         !this.excludedUsers.includes(c.login) &&
-        !ctbtApp.some((c2) => c2.login === c.login)
+        !ctbtApp.some(c2 => c2.login === c.login)
     );
-    ctbtWebsite.forEach((el) => {
+    ctbtWebsite.forEach(el => {
       this.addContributor({
         login: el.login,
         html_url: el.html_url,
@@ -174,9 +158,10 @@ export default {
         app: false,
         website: true,
         customSentence: "",
+        require_image: false
       });
     });
-  },
+  }
 };
 </script>
 
