4bd52af11fdf266121190323ecd4d792222257eb
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2021-10-26 16:38:35 +0200

    update banner color

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,5 +1,5 @@
 <template>
-  <a :href="require('~/assets/content/contact.json').tipeee" target="_blank" rel="noopener noreferrer" class="p-2 bg-indigo-800 text-center text-white font-medium hover:bg-indigo-700 transition-colors group">
+  <a :href="require('~/assets/content/contact.json').tipeee" target="_blank" rel="noopener noreferrer" class="p-2 bg-space-400 text-center text-white font-medium hover:bg-space-300 transition-colors group">
     Nous avons maintenant un Tipeee ! <span class="group-hover:underline">En savoir plus</span>
   </a>
 </template>
