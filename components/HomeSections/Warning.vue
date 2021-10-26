565a09966c68413ccdf365a9179f8c1fe44ba436
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2021-10-26 16:36:28 +0200

    close #16

--- components/HomeSections/Warning.vue
+++ components/HomeSections/Warning.vue
@@ -3,16 +3,12 @@
     <Container>
       <XyzTransition appear-visible duration="auto">
         <div
-          class="relative px-4 py-8 border-l-8 border-red-600 bg-gray-100 rounded-xl lg:px-8 text-gray-800 shadow"
+          class="px-2 py-4 bg-gray-100 rounded-lg lg:px-4 text-gray-600"
           role="alert"
           xyz="fade scale delay-2 duration-8"
         >
-          <span
-            class="absolute px-2 bg-red-600 rounded-md shadow -top-3 right-10 text-red-50"
-            v-html="content.title"
-          ></span>
           <p
-            class="font-medium text-justify lg:text-lg"
+            class="font-medium text-justify text-base"
             v-html="content.text"
           ></p>
         </div>
